61e6c12baae45bc68d1cc723a7612e77a3264a4a
Ehren Lillies <ehren@outlook.com>
2017-07-14 20:55:40 -0400

    Able to get data from muse-io

--- WinMuseEEG/Program.cs
+++ WinMuseEEG/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpOSC;
 
 namespace WinMuseEEG
 {
@@ -10,6 +11,36 @@ namespace WinMuseEEG
 	{
 		static void Main(string[] args)
 		{
+			// Callback function for received OSC messages. 
+			// Prints EEG and Relative Alpha data only.
+			HandleOscPacket callback = delegate (OscPacket packet)
+			{
+				var messageReceived = (OscMessage)packet;
+				var addr = messageReceived.Address;
+				if (addr == "/muse/eeg")
+				{
+					Console.Write("EEG values: ");
+					foreach (var arg in messageReceived.Arguments)
+					{
+						Console.Write(arg + " ");
+					}
+				}
+				if (addr == "/muse/elements/alpha_relative")
+				{
+					Console.Write("Relative Alpha power values: ");
+					foreach (var arg in messageReceived.Arguments)
+					{
+						Console.Write(arg + " ");
+					}
+				}
+			};
+
+			// Create an OSC server.
+			var listener = new UDPListener(5000, callback);
+
+			Console.WriteLine("Press enter to stop");
+			Console.ReadLine();
+			listener.Close();
 		}
 	}
 }
---
+++ WinMuseEEG/Readme.txt
@@ -0,0 +1,2 @@
+﻿Muse connection command for Muse-201E:
+	muse-io --device Muse-201E --osc osc.udp://localhost:5000
